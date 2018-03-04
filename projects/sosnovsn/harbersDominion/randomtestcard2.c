6d8d1c72e5579c252aa87f17ba8e69fb900205a0
Nathan Sosnovske <nathan@sosnovske.com>
2018-03-04 12:14:40 -0800

    Add random testing utilities

---
+++ projects/sosnovsn/harbersDominion/randomtestcard2.c
@@ -0,0 +1,60 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "randomTestingUtilities.h"
+#include <stdio.h>
+#include <math.h>
+#include "rngs.h"
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+int returnErrorCount = 0;
+
+int iteration = 0;
+
+
+void checkGardens()
+{
+    struct gameState dummyState;
+    GenerateGameState(&dummyState, 1, 4);
+
+    int ret = cardEffect(gardens, 0, 0, 0, &dummyState, 0, 0);
+    // int ret = apply_gardens();
+    
+    if(ret == -1)
+    {
+    
+    }
+    else
+    {
+        printf("Fail!!! Gardens returned %i. Expected %i\n", ret, -1);
+        returnErrorCount++;
+    }
+}
+
+int main()
+{
+  srand(time(NULL));
+  SelectStream(2);
+  PutSeed(3);
+
+  iteration = 0;
+
+  printf("-- RANDOM TEST: GARDENS CARD --\n");
+
+  for(iteration = 0; iteration < 1; iteration++)
+  {
+    checkGardens();
+  }
+
+  if(returnErrorCount == 0)
+  {
+    printf("All tests passed!\n");
+  }
+  else
+  {
+    printf("Return Error Count: %i\n", returnErrorCount);
+  }
+
+  return 0;
+}
